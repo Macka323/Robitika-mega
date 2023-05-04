0b9a23f7be1f58cf0432016064b94565422b4f7a
Macka323 <martin.milchevski2@gmail.com>
2023-05-04 17:20:26 +0200

    *

---
+++ Robotika-mega/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  Serial2.begin(115200);
+  Serial3.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
